dc2cbb53fc6b8b70c18fce41725e6ba4d11ecd5e
PaPka_B_Kedax <serezha.kalinin.2005@mail.ru>
2025-12-19 20:55:51 +0300

    Финал

--- main_parser.py
+++ main_parser.py
@@ -51,11 +51,11 @@ def setup_logging():
         mode='w', 
         encoding='utf-8'
     )
-    file_handler.setLevel(logging.DEBUG)
+    file_handler.setLevel(logging.DEBUG) 
     file_format = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     file_handler.setFormatter(file_format)
     
-    # проверка
+    # Проверка
     logger.addHandler(console_handler)
     logger.addHandler(file_handler)
 
